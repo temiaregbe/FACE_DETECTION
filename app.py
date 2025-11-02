a367262f0c6458019fc8ed169b2a198bbcf91a03
Temiloluwa Aregbesola <loluwa06@example.com>
2025-11-02 22:53:30 +0100

    Fixed Render port binding issue

--- app.py
+++ app.py
@@ -54,5 +54,9 @@ def upload():
     return render_template('index.html', message=message)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
